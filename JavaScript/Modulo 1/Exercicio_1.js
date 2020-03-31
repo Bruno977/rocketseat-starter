55478f100287ebf2a60c04aa4aad9ca4e969c76d
Bruno <bruno_151997@hotmail.com>
2020-03-30 23:40:32 -0300

    Iniciando curso JavaScript Rocketseat

---
+++ JavaScript/Modulo 1/Exercicio_1.js
@@ -0,0 +1,8 @@
+let endereco ={
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+};
+console.log(`O usuario mora em ${endereco.cidade} / ${endereco.uf}, no bairro ${endereco.bairro}, na rua ${endereco.rua} com o numero n ${endereco.numero}`)
---
+++ JavaScript/Modulo 1/Exercicio_2.js
@@ -0,0 +1,9 @@
+function pares(x, y){
+    while (x <= y){
+        if (x % 2 === 0) {
+            console.log(`Par: ${x}`);
+        };
+        x+=1
+    };
+};
+pares(32, 321)
